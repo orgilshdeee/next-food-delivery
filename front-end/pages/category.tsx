7611fbe6564cefb3ee355160feea4546d4acac10
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-08-02 10:59:22 +0800

    push

---
+++ front-end/pages/category.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+const category = () => {
+  return (
+    <div>
+      <h1>All categories page</h1>
+      <Button variant="contained">Towch</Button>
+    </div>
+  );
+};
+
+export default category;
